3b4b7d69bb7a7c865710b013733b5b29a47a5cfc
EmmaAntuna1 <88903845+EmmaAntuna1@users.noreply.github.com>
2021-10-19 13:29:17 -0500

    added the github repo url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+  <div className="App">
+    Weather App
+    <footer>
+      This project was coded by Emma Antuna and is{" "}
+    <a href="https://github.com/EmmaAntuna1/weather-app-react"
+    target="_blank"
+    >
+       open-sourced on github
+    </a>
+    </footer>
+  </div>
+  );
 }
 
 
